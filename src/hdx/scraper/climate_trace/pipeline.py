26d8243673cbc9899ba9b538f47df48c3ce8ea8a
Briar <briar.j.mills@gmail.com>
2026-02-27 19:31:36 -0500

    Dev changes (#1)
    * Switch to uv
    
    * Refactor optional dependencies into dependency groups
    
    * Add metadata
    
    * Write download function
    
    * Prototype complete
    
    * Add test fixtures
    
    * Get all data at admin 0, 1, and city levels
    
    * Update test fixtures
    
    * Write first tests
    
    * Update uv lock
    
    * Add coverage and test files
    
    * Move dates to pipeline object
    
    * Add sources data
    
    * Add file comparison
    
    * Metadata changes
    
    * Change gas name
    
    * Change back gas name
    
    * List gases by level
    
    * Swap pm2.5 for methane
    
    ---------
    
    Co-authored-by: Mike <mcarans@yahoo.co.uk>

--- src/hdx/scraper/climate_trace/pipeline.py
+++ src/hdx/scraper/climate_trace/pipeline.py
@@ -2,31 +2,149 @@
 """Climate_trace scraper"""
 
 import logging
-from typing import Optional
+from datetime import datetime
 
+from dateutil.relativedelta import relativedelta
 from hdx.api.configuration import Configuration
 from hdx.data.dataset import Dataset
-from hdx.data.hdxobject import HDXError
+from hdx.location.country import Country
+from hdx.utilities.base_downloader import DownloadError
 from hdx.utilities.retriever import Retrieve
 
 logger = logging.getLogger(__name__)
 
 
 class Pipeline:
-    def __init__(self, configuration: Configuration, retriever: Retrieve, tempdir: str):
+    def __init__(
+        self,
+        configuration: Configuration,
+        retriever: Retrieve,
+        tempdir: str,
+        today: datetime,
+    ):
         self._configuration = configuration
         self._retriever = retriever
         self._tempdir = tempdir
+        self.today = today
+        self.min_date = today - relativedelta(years=2)
+        self.admins = {}
+        self.data = {}
 
-    def generate_dataset(self) -> Optional[Dataset]:
-        # To be generated
-        dataset_name = None
-        dataset_title = None
-        dataset_time_period = None
-        dataset_tags = None
-        dataset_country_iso3 = None
+    def get_admin_data(self, iso3s: list[str]) -> None:
+        for iso3 in iso3s:
+            country_name = Country.get_country_name_from_iso3(iso3)
+            admin_info = [
+                {
+                    "full_name": country_name,
+                    "id": iso3,
+                    "level": 0,
+                    "level_0_id": iso3,
+                    "level_1_id": "",
+                    "level_2_id": "",
+                    "name": country_name,
+                }
+            ]
+            admin1_url = self._configuration["admin_url"].format(admin_id=iso3)
+            try:
+                admin1_json = self._retriever.download_json(admin1_url)
+            except DownloadError:
+                logger.warning(f"No admin units found for {iso3}")
+                admin1_json = []
+            admin_info.extend(admin1_json)
+            city_url = self._configuration["city_url"].format(admin_id=iso3)
+            try:
+                city_json = self._retriever.download_json(city_url)
+            except DownloadError:
+                logger.warning(f"No cities found for {iso3}")
+                city_json = []
+            self.admins[iso3] = {"admin": admin_info, "city": city_json}
+        return
 
-        # Dataset info
+    def process_emissions_admin_rows(
+        self, input_data: dict, admin_unit: dict
+    ) -> list[dict]:
+        rows = []
+        min_year = self.min_date.year
+        min_month = self.min_date.month
+        sector_data = input_data["sectors"]["timeseries"]
+        if sector_data is None:
+            sector_data = []
+        subsector_data = input_data["subsectors"]["timeseries"]
+        if subsector_data is None:
+            subsector_data = []
+        for row in sector_data + subsector_data:
+            if row["year"] <= min_year and row["month"] < min_month:
+                continue
+            new_row = admin_unit | row
+            rows.append(new_row)
+        return rows
+
+    def process_emissions_source_rows(self, input_data: list[dict]) -> list[dict]:
+        rows = []
+        min_year = self.min_date.year
+        for row in input_data:
+            if row["year"] < min_year:
+                continue
+            rows.append(row)
+        return rows
+
+    def get_emissions_admin_data(self) -> None:
+        min_year = self.min_date.year
+        max_year = self.today.year
+
+        # loop through countries, admin units, gases, and years
+        base_url = self._configuration["emissions_url"]
+        for iso3, admin_types in self.admins.items():
+            self.data[iso3] = {}
+            for admin_type, admin_units in admin_types.items():
+                if admin_type == "admin":
+                    admin_id_type = "gadmId"
+                else:
+                    admin_id_type = "cityId"
+                for gas in self._configuration["gases"][admin_type]:
+                    self.data[iso3][f"{gas}|{admin_type}"] = []
+                    for admin_unit in admin_units:
+                        admin_id = admin_unit["id"]
+                        for year in range(min_year, max_year + 1):
+                            url = f"{base_url}?year={year}&gas={gas}&{admin_id_type}={admin_id}"
+                            json = self._retriever.download_json(url)
+                            rows = self.process_emissions_admin_rows(json, admin_unit)
+                            self.data[iso3][f"{gas}|{admin_type}"].extend(rows)
+        return
+
+    def get_emissions_source_data(self) -> None:
+        min_year = self.min_date.year
+        max_year = self.today.year
+
+        # loop through countries, gases, sectors, and years
+        base_url = self._configuration["source_url"]
+        for iso3, _ in self.admins.items():
+            if iso3 not in self.data:
+                self.data[iso3] = {}
+            for gas in self._configuration["gases"]["source"]:
+                self.data[iso3][f"{gas}|source"] = []
+                for sector in self._configuration["sectors"]:
+                    for year in range(min_year, max_year + 1):
+                        for page in range(10000):
+                            url = f"{base_url}?sectors={sector}&year={year}&gas={gas}&gadmId={iso3}&limit=10000&offset={page * 10000}"
+                            json = self._retriever.download_json(url)
+                            if json is None:
+                                break
+                            rows = self.process_emissions_source_rows(json)
+                            self.data[iso3][f"{gas}|source"].extend(rows)
+                            if len(json) < 10000:
+                                break
+        return
+
+    def generate_country_dataset(self, iso3: str) -> Dataset | None:
+        country_name = Country.get_country_name_from_iso3(iso3)
+        dataset_name = f"{iso3.lower()}-climate-trace"
+        dataset_title = f"{country_name}: {self._configuration['dataset_title']}"
+
+        country_data = self.data.get(iso3)
+        if country_data is None:
+            return None
+
         dataset = Dataset(
             {
                 "name": dataset_name,
@@ -34,16 +152,56 @@ class Pipeline:
             }
         )
 
-        dataset.set_time_period(dataset_time_period)
-        dataset.add_tags(dataset_tags)
-        # Only if needed
-        dataset.set_subnational(True)
-        try:
-            dataset.add_country_location(dataset_country_iso3)
-        except HDXError:
-            logger.error(f"Couldn't find country {dataset_country_iso3}, skipping")
-            return
+        subnational = False
+        dates = set()
+        for data_type, rows in country_data.items():
+            if len(rows) == 0:
+                continue
+            gas, admin_type = data_type.split("|")
+            admin_levels = set()
+            for row in rows:
+                if "month" in row:
+                    date = f"{row['year']}-{str(row['month']).zfill(2)}"
+                    dates.add(date)
+                else:
+                    dates.add(f"{row['year']}-1")
+                    dates.add(f"{row['year']}-12")
+                if admin_type == "admin":
+                    admin_levels.add(str(row["level"]))
+                if not subnational:
+                    if admin_type in ["city", "source"] or (
+                        admin_type == "admin" and row["level"] > 0
+                    ):
+                        subnational = True
+            admin_name = admin_type
+            admin_desc = admin_type
+            if admin_type == "admin":
+                admin_name = f"admin_{'_'.join(sorted(list(admin_levels)))}"
+                admin_desc = f"admin {' and '.join(sorted(list(admin_levels)))}"
+            gas_desc = self._configuration["gas_names"][gas]
+            resource_name = f"{iso3.lower()}_{gas}_{admin_name}.csv"
+            resource_info = {
+                "name": resource_name,
+                "description": f"{country_name} {gas_desc} emissions over the past 2 years at the {admin_desc} level.",
+            }
 
-        # Add resources here
+            dataset.generate_resource(
+                self._tempdir,
+                resource_info["name"],
+                rows,
+                resource_info,
+            )
+
+        start_date = f"{min(dates)}-1"
+        end_year, end_month = max(dates).split("-")
+        end_date = datetime(int(end_year), int(end_month), 1)
+        end_date = end_date + relativedelta(months=1) - relativedelta(days=1)
+        if end_date > self.today.replace(tzinfo=None):
+            end_date = self.today.replace(tzinfo=None)
+        dataset.set_time_period(start_date, end_date)
+        dataset.add_tags(self._configuration["tags"])
+
+        dataset.set_subnational(subnational)
+        dataset.add_country_location(iso3)
 
         return dataset
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from os.path import join
 
+from hdx.utilities.compare import assert_files_same
 from hdx.utilities.downloader import Download
 from hdx.utilities.path import temp_dir
 from hdx.utilities.retriever import Retrieve
@@ -23,8 +25,90 @@ class TestPipeline:
                     save=False,
                     use_saved=True,
                 )
-                pipeline = Pipeline(configuration, retriever, tempdir)
-                dataset = pipeline.generate_dataset()
+                today = datetime(2026, 2, 15)
+                pipeline = Pipeline(configuration, retriever, tempdir, today)
+                pipeline.get_admin_data(["AFG"])
+                pipeline.get_emissions_admin_data()
+                pipeline.get_emissions_source_data()
+
+                dataset = pipeline.generate_country_dataset("AFG")
                 dataset.update_from_yaml(
                     path=join(config_dir, "hdx_dataset_static.yaml")
                 )
+                assert dataset == {
+                    "name": "afg-climate-trace",
+                    "title": "Afghanistan: Greenhouse Gas and Air Pollutant Emissions",
+                    "dataset_date": "[2024-02-01T00:00:00 TO 2025-12-31T23:59:59]",
+                    "tags": [
+                        {
+                            "name": "climate-weather",
+                            "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1",
+                        },
+                        {
+                            "name": "environment",
+                            "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1",
+                        },
+                        {
+                            "name": "points of interest-poi",
+                            "vocabulary_id": "b891512e-9516-4bf5-962a-7a289772a2a1",
+                        },
+                    ],
+                    "subnational": "1",
+                    "groups": [{"name": "afg"}],
+                    "license_id": "cc-by",
+                    "methodology": "Other",
+                    "methodology_other": "Climate TRACE identifies the most significant gaps in emissions reporting within current greenhouse gas inventories and develops models to estimate those emissions gaps. Climate TRACE emissions data is generated on a sector-by-sector basis by the relevant Climate TRACE sector lead and additional contributed organizations they collaborate with.\n\nEach Climate TRACE sector uses a methodology that is specialized and relevant to each particular sector to estimate emissions. The most common method is using computer vision models to combine one predictor variable (such as satellite imagery) and one high-quality source of ground truth data (such as verified, calibrated sensors located at individual facilities). Computer vision models are a form of artificial intelligence that, unlike generative AI, is relatively easy to verify and not prone to hallucination. In addition, Climate TRACE also uses a number of other statistical approaches such as disaggregating total production within a country to all the facilities, and modeling emissions using IPCC National GHG Inventory Guidelines.\n\nWe’ve published detailed documentation on our methodologies, peer reviewed research that underlies those methodologies, and the key datasets and assumptions for the Climate TRACE inventory. You can find those on our data downloads page at https://climatetrace.org/data.\n",
+                    "caveats": "See methodology documents for sector specific caveats",
+                    "dataset_source": "Climate TRACE",
+                    "package_creator": "HDX Data Systems Team",
+                    "private": False,
+                    "maintainer": "8c24c8ce-f313-4020-bda6-00ae42b2aef1",
+                    "owner_org": "climate-trace",
+                    "data_update_frequency": 30,
+                    "notes": "Climate TRACE is a non-profit coalition of organizations building a timely, open, and accessible inventory of exactly where greenhouse gas emissions are coming from. Climate TRACE estimates greenhouse gas (GHG) and air pollutant emissions for over 2.7 million sources (from over 744 million assets), and every single country globally.\n\nThe Climate TRACE emissions inventory includes:\n- Annual country-level emissions by sub-sector and by gas beginning in 2015\n- Monthly source-level emissions by sub-sector and gas beginning in 2021 and confidence\n- Emissions source ownership where and when available.\n",
+                }
+                resources = dataset.get_resources()
+                assert resources == [
+                    {
+                        "name": "afg_ch4_admin_0_1.csv",
+                        "description": "Afghanistan methane emissions over the past 2 years at the admin 0 and 1 level.",
+                        "format": "csv",
+                    },
+                    {
+                        "name": "afg_co2e_20yr_admin_0_1.csv",
+                        "description": "Afghanistan carbon dioxide equivalent (CO2e) on a 20-year time horizon emissions over the past 2 years at the admin 0 and 1 level.",
+                        "format": "csv",
+                    },
+                    {
+                        "name": "afg_ch4_city.csv",
+                        "description": "Afghanistan methane emissions over the past 2 years at the city level.",
+                        "format": "csv",
+                    },
+                    {
+                        "name": "afg_co2e_20yr_city.csv",
+                        "description": "Afghanistan carbon dioxide equivalent (CO2e) on a 20-year time horizon emissions over the past 2 years at the city level.",
+                        "format": "csv",
+                    },
+                    {
+                        "name": "afg_ch4_source.csv",
+                        "description": "Afghanistan methane emissions over the past 2 years at the source level.",
+                        "format": "csv",
+                    },
+                    {
+                        "name": "afg_co2e_20yr_source.csv",
+                        "description": "Afghanistan carbon dioxide equivalent (CO2e) on a 20-year time horizon emissions over the past 2 years at the source level.",
+                        "format": "csv",
+                    },
+                    {
+                        "name": "afg_pm2_5_source.csv",
+                        "description": "Afghanistan PM2.5 emissions over the past 2 years at the source level.",
+                        "format": "csv",
+                    },
+                ]
+
+                for resource in resources:
+                    file_name = resource["name"]
+                    assert_files_same(
+                        join(fixtures_dir, file_name),
+                        join(tempdir, file_name),
+                    )
